b0ac16be1c2cdf23552ce26389899cda2468e07f
sabokoha <131151103+sabokoha@users.noreply.github.com>
2024-10-02 20:03:04 +0900

    chk

--- TsWeb2/Build/WebGLF.framework.js
+++ TsWeb2/Build/WebGLF.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3197584: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3197645: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3197709: function() {return Module.webglContextAttributes.powerPreference;},  
- 3197767: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3197822: function($0) {performance.now = function() { return $0; };},  
- 3197870: function($0) {performance.now = function() { return $0; };},  
- 3197918: function() {performance.now = Module['emscripten_get_now_backup'];}
+  3197616: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3197677: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3197741: function() {return Module.webglContextAttributes.powerPreference;},  
+ 3197799: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3197854: function($0) {performance.now = function() { return $0; };},  
+ 3197902: function($0) {performance.now = function() { return $0; };},  
+ 3197950: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
